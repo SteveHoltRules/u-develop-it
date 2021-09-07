cc08b56cd6a05b06e9b613d1ae4d48a4e9a322b3
Matt Luna <mat.c.luna@gmail.com>
2021-09-06 22:23:38 -0500

    Select all query

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,14 +8,25 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connet to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'Txn@lgene11',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello World'
-  });
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
 });
 
 // Default response for any other request (Not Found)
