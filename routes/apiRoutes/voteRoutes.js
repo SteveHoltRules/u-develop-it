29f70eb93184ba7e004dc2db38aa0a4a1fd0ea08
Matt Luna <mat.c.luna@gmail.com>
2021-10-17 00:37:31 -0500

    feature/votes

--- routes/apiRoutes/voteRoutes.js
+++ routes/apiRoutes/voteRoutes.js
@@ -27,4 +27,29 @@ router.post('/vote', ({ body }, res) => {
   });
 });
 
+router.get('/vote', ({ body }, res) => {
+  //Data validation
+  const errors = inputCheck(body, 'voter_id', 'candidate_id');
+  if(errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `SELECT candidates.*, parties.name AS party_name, COUNT(candidate_id) FROM votes 
+  LEFT JOIN candidates ON votes.candidate_id = candidates.id 
+  LEFT JOIN parties ON candidates.party_id = parties.id GROUP BY candidate_id`;
+
+  db.query(sql, params, (err, result) => {
+    if(err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body,
+      changes: result.affectedRows
+    });
+  });
+});
+
 module.exports = router;
